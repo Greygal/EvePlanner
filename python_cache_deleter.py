ca449adfe7fc131474ccf3ddb7570e8084b68948
stkiller <golovorub@gmail.com>
2013-06-03 06:00:48 +0300

    Deleted non-needed output.

--- python_cache_deleter.py
+++ python_cache_deleter.py
@@ -4,6 +4,7 @@ import shutil
 __author__ = 'stkiller'
 
 dir_name = os.path.dirname(os.path.realpath(__file__))
+print(dir_name)
 for (root, dirs, files) in os.walk(dir_name, topdown=False):
     for current_dir in dirs:
         path = os.path.join(root, current_dir)
